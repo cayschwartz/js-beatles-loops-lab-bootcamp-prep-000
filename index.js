72a7839defef3a3c859092d3f3a077bab13bb08c
Cayla Schwartz <cayschwartz@gmail.com>
2017-09-12 17:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var temp = []
   for(var i=0; i<=musicians.length; i++){
-    temp.push(${musicians[i]}+" plays "+ ${instruments[i]} )
+    temp.push(`${musicians[i]} plays ${instruments[i]}` )
   }
   return temp
 
